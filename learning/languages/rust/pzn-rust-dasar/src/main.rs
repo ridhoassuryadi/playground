3eacdbd4db1ad0d666564f9f10217c118841b4cd
Ridho Assuryadi <ulunmuhammadridho@gmail.com>
2025-04-20 07:53:20 +0700

    feat: pzn rust dasar mutable and variable

---
+++ learning/languages/rust/pzn-rust-dasar/src/main.rs
@@ -0,0 +1,41 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[test]
+fn hello_test() {
+    println!("hello there")
+}
+
+#[test]
+fn test_mutable() {
+    let mut name = "Eko Kurniawan Kennedy";
+    println!("{}", name);
+
+    name = "Ridho Assuryadi";
+    println!("{}", name);
+}
+
+
+struct Person {
+   first_name: String,
+   middle_name: String,
+   last_name: String,
+   age: u8 
+}
+
+#[test]
+fn get_person() {
+    let person: Person = Person{
+        first_name: String::from("Muhammad"),
+        middle_name: String::from("Ridho"),
+        last_name: String::from("Assuryadi"),
+        age: 20
+    };
+
+    println!("{}", person.first_name);
+    println!("{}", person.middle_name);
+    println!("{}", person.last_name);
+    println!("{}", person.age);
+}
+
---
+++ learning/languages/rust/pzn-rust-dasar/target/debug/deps/pzn_rust_dasar-19fadb5023e0fb12.d
@@ -0,0 +1,5 @@
+/Users/ridho/Documents/go/github.com/ridhoassuryadi/playground/learning/languages/rust/pzn-rust-dasar/target/debug/deps/pzn_rust_dasar-19fadb5023e0fb12: src/main.rs
+
+/Users/ridho/Documents/go/github.com/ridhoassuryadi/playground/learning/languages/rust/pzn-rust-dasar/target/debug/deps/pzn_rust_dasar-19fadb5023e0fb12.d: src/main.rs
+
+src/main.rs:
